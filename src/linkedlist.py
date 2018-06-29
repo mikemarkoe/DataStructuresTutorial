b6f22c16d444f23eb6bbd3fce70db9ca5bf57ef2
Michael Markoe <mikemarkoe@hotmail.com>
2018-06-28 21:09:10 -0700

    add an add_node_after() method to the node class

--- src/linkedlist.py
+++ src/linkedlist.py
@@ -14,8 +14,8 @@ class StringLinkedNode:
     data = ""
     link = None
     
-    def __init__(self, data = "", link = None):
-        self.data = data
+    def __init__(self, link = None, node_data = ""):
+        self.data = node_data
         self.link = link
         
     def get_data(self):
@@ -30,11 +30,22 @@ class StringLinkedNode:
     def set_link(self, updated_link = None):
         self.link = updated_link
         
+    def add_node_after(self, node_data):
+        ''' Add a node after this node.
+            If it is "None" it will become the tail of the list.
+        '''
+        current_next_node = self.get_link()
+        self.link = StringLinkedNode(current_next_node, node_data)
 
 
 if __name__ == '__main__':
     
-    head = StringLinkedNode("this is the HEAD", None)
-    
+    next2 = StringLinkedNode(None, "Next Next one")        
+    next1 = StringLinkedNode(next2, "Next one")
+    head = StringLinkedNode(next1, "this is the HEAD")
+    head = StringLinkedNode(head, "the new head")
+    head.add_node_after("I'm new data, I just jumped in here!")
     print(head.get_data())
+    print(head.get_link().get_data())
+    print(head.get_link().get_link().get_data())
     pass
