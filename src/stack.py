3ef6878982ab26f4ef58c3e8e3e28054b6d89d2f
Michael Markoe <mikemarkoe@hotmail.com>
2018-07-10 07:45:19 -0700

    created class and empty methods for adding the stack data structure

---
+++ src/stack.py
@@ -0,0 +1,50 @@
+'''
+Created on Jul 9, 2018
+
+@author: fantana
+
+This module represents chapter six of Data Structures and Other Objects in Java.
+
+My purpose is to convert the data structures class that I learned in college into
+the Python language.
+'''
+from ctypes.test.test_sizes import SizesTestCase
+
+class StringStack:
+    def __init__(self)
+    ''' this initializes an empty stack
+    '''
+    
+    def is_empty(self):
+        ''' returns true if the stack is empty
+        '''
+        #todo write code
+        return True
+    
+    def peek(self):
+        ''' return the string at the top of the stack
+        '''
+        #todo write code
+        return ""
+    
+    def pop(self):
+        ''' return the string at the top of the stack and 
+            remove it from the stack.
+        '''
+        #todo write code
+        return ""
+    
+    def push(self, string_to_add):
+        ''' Add a string to the top of the stack
+        '''
+        #todo write code
+        
+    def size(self):
+        ''' return the number of items in the stack as an int
+        '''
+        size = 0
+        #todo write code
+        return size
+
+if __name__ == '__main__':
+    pass
